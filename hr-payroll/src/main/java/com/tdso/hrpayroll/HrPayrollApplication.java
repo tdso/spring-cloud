6dfbbad918b4592535a070d182fb1a4922bfabc4
tdso <tdso.oliveira@gmail.com>
2021-11-19 16:11:11 -0300

    config servidor eureka e config das ops como client do eureka server

--- hr-payroll/src/main/java/com/tdso/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/tdso/hrpayroll/HrPayrollApplication.java
@@ -2,12 +2,14 @@ package com.tdso.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@RibbonClient(name="hr-worker")
+//@RibbonClient(name="hr-worker") - eureka ja tem um load balance
+@EnableEurekaClient
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
